e09e953902118876e441160a0ebeace24f1fb997
Denis <denisproper3859@gmail.com>
2025-03-08 17:11:37 +0300

    added headless mode

--- conftest.py
+++ conftest.py
@@ -8,6 +8,7 @@ from selenium.webdriver.chrome.options import Options
 def browser():
     options = Options()
     options.add_argument("--window-size=1920, 1080")
+    options.add_argument("--headless")
     options.add_argument("--disable-blink-features=AutomationControlled")
     options.add_argument("--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Safari/537.3")
     print("\nstart browser for test..")
