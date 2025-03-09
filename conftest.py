b8aa852d87da9169323e608dc0357440dbee6bb0
Denis <denisproper3859@gmail.com>
2025-03-09 15:01:49 +0300

    new conftest

--- conftest.py
+++ conftest.py
@@ -9,12 +9,12 @@ def browser():
     options = Options()
     options.add_argument("--window-size=1920, 1080")
     options.add_argument("--headless")
+    options.add_argument("--no-sandbox")
     options.add_argument("--disable-blink-features=AutomationControlled")
     options.add_argument("--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Safari/537.3")
     print("\nstart browser for test..")
     browser = webdriver.Chrome(options=options)
     yield browser
     print("\nquit browser..")
-    time.sleep(20)
     browser.quit()
 
--- pages/base_page.py
+++ pages/base_page.py
@@ -2,78 +2,74 @@ import time
 
 import allure
 from selenium.common.exceptions import NoSuchElementException, TimeoutException
-from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
 
 from pages.locators import AuthorizationLocators
 
 class BasePage():
-    def __init__(self, browser, url, timeout=3):
+    def __init__(self, browser, url, timeout=10):
         self.browser = browser
         self.url = url
-        self.browser.implicitly_wait(timeout)
+        self.wait = WebDriverWait(self.browser, timeout, poll_frequency=1)
 
 
-    with allure.step("Открытие страницы"):
-        def open(self):
-            self.browser.get(self.url)
+    @allure.step("Открытие страницы")
+    def open(self):
+        self.browser.get(self.url)
 
 
-    with allure.step("Проверка отображения элемента"):
-        def is_element_present(self, how, what):
-            try:
-                self.browser.find_element(how, what)
-            except (NoSuchElementException):
-                return False
+    @allure.step("Проверка отображения элемента")
+    def is_element_present(self, how, what):
+        try:
+            self.wait.until(EC.presence_of_element_located((how, what)))
             return True
+        except TimeoutException:
+            return False
 
 
-    with allure.step("Клик на элемент"):
-        def click(self, how, what):
-            try:
-                element = WebDriverWait(self.browser, 10).until(
-                    EC.element_to_be_clickable((how, what))
-                )
-                element.click()
-            except (NoSuchElementException, TimeoutException):
-                return False
-
-
-    with allure.step("Получение значения из элемента"):
-        def get_value_by_attribute(self, how, what):
-            return self.browser.find_element(how, what).get_attribute("value")
-
-
-    with allure.step("Получение текста из элемента"):
-        def get_text_from_input(self, how, what):
-            return self.browser.find_element(how, what).text
-
-
-    with allure.step("Заполнение полей email и password"):
-        def fill_email_and_password_fields(self, email, password):
-            with allure.step("Заполнение email поля"):
-                email_field = self.browser.find_element(*AuthorizationLocators.EMAIL_INPUT)
-                email_field.clear()
-                email_field.send_keys(email)
-
-            with allure.step("Заполнение password поля"):
-                password_field = self.browser.find_element(*AuthorizationLocators.PASSWORD_INPUT)
-                password_field.clear()
-                password_field.send_keys(password)
-                self.browser.find_element(*AuthorizationLocators.LOGIN_BUTTON).click()
-
-
-    def scroll_to_element(self, element):
-        if element is not None:
-            self.browser.execute_script("arguments[0].scrollIntoView();", element)
-        else:
-            raise ValueError("Element is None")
-
-
-    def scroll_and_click(self, element):
-        if element is not None:
-            self.browser.execute_script("arguments[0].scrollIntoView(true);", element)
-            time.sleep(0.5)
+    @allure.step("Клик на элемент")
+    def click(self, how, what):
+        try:
+            element = self.wait.until(EC.element_to_be_clickable((how, what)))
             element.click()
-        else:
-            raise ValueError("Element is None")
+        except TimeoutException:
+            raise TimeoutException(f"Не удалось кликнуть по элементу {what}")
+
+
+    @allure.step("Получение значения из элемента")
+    def get_value_by_attribute(self, how, what, attribute="value"):
+        element = self.wait.until(EC.presence_of_element_located((how, what)))
+        return element.get_attribute(attribute)
+
+
+    @allure.step("Получение текста из элемента")
+    def get_text_from_input(self, how, what):
+        element = self.wait.until(EC.presence_of_element_located((how, what)))
+        return element.text
+
+
+    @allure.step("Заполнение полей email и password")
+    def fill_email_and_password_fields(self, email, password):
+        email_field = self.wait.until(EC.visibility_of_element_located(AuthorizationLocators.EMAIL_INPUT))
+        email_field.clear()
+        email_field.send_keys(email)
+
+        password_field = self.wait.until(EC.visibility_of_element_located(AuthorizationLocators.PASSWORD_INPUT))
+        password_field.clear()
+        password_field.send_keys(password)
+
+        login_button = self.wait.until(EC.element_to_be_clickable(AuthorizationLocators.LOGIN_BUTTON))
+        login_button.click()
+
+
+    def scroll_to_element(self, how, what):
+        element = self.wait.until(EC.presence_of_element_located((how, what)))
+        self.browser.execute_script("arguments[0].scrollIntoView();", element)
+
+
+    def scroll_and_click(self, how, what):
+        element = self.wait.until(EC.element_to_be_clickable((how, what)))
+        self.browser.execute_script("arguments[0].scrollIntoView(true);", element)
+        time.sleep(0.5)
+        element.click()
