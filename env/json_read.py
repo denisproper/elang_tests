3410f7f62ac887bdac553fce32a1cc4d41c246e5
Denis <denisproper3859@gmail.com>
2025-03-09 14:53:11 +0300

    added secret variables

--- env/json_read.py
+++ env/json_read.py
@@ -9,14 +9,19 @@ with open(config_path, 'r') as file:
     config = json.load(file)
 
 
+valid_email = os.getenv("VALID_EMAIL")
+valid_password = os.getenv("VALID_PASSWORD")
+
+if not valid_email or not valid_password:
+    raise ValueError("Переменные окружения VALID_EMAIL и VALID_PASSWORD не заданы!")
+
+
 link = config['link']
-valid_email = config['valid_email']
 empty_email = config['empty_email']
 digits_email = config['digits_email']
 digits_email_with_at = config['digits_email_with_at']
 digits_email_with_at_com = config['digits_email_with_at_com']
 partly_right_email = config['partly_right_email']
-valid_password = config['valid_password']
 empty_password = config['empty_password']
 special_symbols_password = config['special_symbols_password']
 digits_password = config['digits_password']
