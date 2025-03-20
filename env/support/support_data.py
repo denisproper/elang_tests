dfa7bcad7a27a7e10c376afe02cda5e04dee23dd
Denis <denisproper3859@gmail.com>
2025-03-20 22:27:59 +0300

    slice for valid name

--- env/support/support_data.py
+++ env/support/support_data.py
@@ -7,12 +7,12 @@ class SupportData:
     @staticmethod
     def get_valid_data():
         return [
-            (fake.first_name(), fake.email(), fake.sentence()),
-            (fake.first_name(), fake.email(), "Программирование — это искусство создания кода, которое решает задачи и автоматизирует процессы. Разработка включает анализ, проектирование, кодирование, тестирование и поддержку. Важно писать чистый и понятный код, следовать принципам SOLID, DRY и KISS. Использование Git помогает контролировать версии, а автоматизированное тестирование гарантирует надёжность. Языки программирования, такие как Python, JavaScript и Java, позволяют реализовать проекты разного уровня. Разработка веб-приложений, мобильных приложений и систем машинного обучения требует знания фреймворков и библиотек. Docker и Kubernetes обеспечивают удобное развертывание сервисов. Современные технологии, такие как облачные вычисления и микросервисы, упрощают масштабирование. Разработчики постоянно учатся, осваивают новые технологии и инструменты. Главное — логика, практика и стремление к совершенствованиюkwjerjwerkjweljrkwejrlkwjelrjwelkrjlkwejlrlwekwerjklwjerkjwelkrjlwejrlkwejlrwerwe,mr,nwemrnw,enr,wne,rwemnrm,wenr,mnew,rn"),
-            (fake.first_name(), fake.email(), fake.paragraph(nb_sentences=5)),
+            (fake.first_name()[:13], fake.email(), fake.sentence()),
+            (fake.first_name()[:13], fake.email(), "Программирование — это искусство создания кода, которое решает задачи и автоматизирует процессы. Разработка включает анализ, проектирование, кодирование, тестирование и поддержку. Важно писать чистый и понятный код, следовать принципам SOLID, DRY и KISS. Использование Git помогает контролировать версии, а автоматизированное тестирование гарантирует надёжность. Языки программирования, такие как Python, JavaScript и Java, позволяют реализовать проекты разного уровня. Разработка веб-приложений, мобильных приложений и систем машинного обучения требует знания фреймворков и библиотек. Docker и Kubernetes обеспечивают удобное развертывание сервисов. Современные технологии, такие как облачные вычисления и микросервисы, упрощают масштабирование. Разработчики постоянно учатся, осваивают новые технологии и инструменты. Главное — логика, практика и стремление к совершенствованиюkwjerjwerkjweljrkwejrlkwjelrjwelkrjlkwejlrlwekwerjklwjerkjwelkrjlwejrlkwejlrwerwe,mr,nwemrnw,enr,wne,rwemnrm,wenr,mnew,rn"),
+            (fake.first_name()[:13], fake.email(), fake.paragraph(nb_sentences=5)),
             (fake.first_name()[:2], fake.email(), fake.sentence()),
             (fake.first_name()[:13], fake.email(), fake.sentence()),
-            (fake.user_name(), fake.email(), fake.sentence())
+            (fake.user_name()[:13], fake.email(), fake.sentence())
         ]
 
     @staticmethod
